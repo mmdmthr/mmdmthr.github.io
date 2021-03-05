156b5c4e74a69389eb5daf3ca13107d97f448c64
unknown <muhhari@gmail.com>
2021-03-05 22:21:16 +0700

    Updates

--- sw.js
+++ sw.js
@@ -27,10 +27,10 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-58066413e799d8209cff.js"
+    "url": "webpack-runtime-27b519d418a57d63f5c2.js"
   },
   {
-    "url": "styles.351daf03d6d3acae997c.css"
+    "url": "styles.cfe82ac6dfddf31d5e41.css"
   },
   {
     "url": "styles-407fe62976dc5310c43e.js"
@@ -43,7 +43,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "9f423372324a74d471f83c913750329d"
+    "revision": "e7bb3c1019ccd2ff526c1d06fa8ff0bf"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-1972652a375cec723dd9.js"
@@ -54,7 +54,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "0b9a94666638f082ac42942c78ba89dd"
+    "revision": "6233cba7588afc82d2b4a2eff51c5cdb"
   },
   {
     "url": "polyfill-0eca59d2ee8c0828e3a6.js"
