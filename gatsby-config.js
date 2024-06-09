ebd8cb633a089b78e97b8929e7bb9665d447b718
mmdmthr <muhhari@gmail.com>
2024-06-09 15:04:54 +0700

    replace devjurnal.com with mmdmthr.github.io

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,129 +1,129 @@
 const metaConfig = require('./gatsby-meta-config')
 
 module.exports = {
-  siteMetadata: metaConfig,
-  plugins: [
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        path: `${__dirname}/content/blog`,
-        name: `blog`,
-      },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        path: `${__dirname}/content/__about`,
-        name: `about`,
-      },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        path: `${__dirname}/content/assets`,
-        name: `assets`,
-      },
-    },
-    {
-      resolve: `gatsby-transformer-remark`,
-      options: {
-        plugins: [
-          {
-            resolve: `gatsby-remark-katex`,
+    siteMetadata: metaConfig,
+    plugins: [
+        {
+            resolve: `gatsby-source-filesystem`,
             options: {
-              strict: `ignore`,
+                path: `${__dirname}/content/blog`,
+                name: `blog`,
             },
-          },
-          {
-            resolve: `gatsby-remark-images`,
-            options: {
-              maxWidth: 1200,
-              linkImagesToOriginal: false,
-            },
-          },
-          {
-            resolve: `gatsby-remark-images-medium-zoom`,
-            options: {
-              margin: 36,
-              scrollOffset: 0,
-            },
-          },
-          {
-            resolve: `gatsby-remark-responsive-iframe`,
-            options: {
-              wrapperStyle: `margin-bottom: 1.0725rem`,
-            },
-          },
-          {
-            resolve: `gatsby-remark-prismjs`,
-            options: {
-              inlineCodeMarker: '%',
-            },
-          },
-          `gatsby-remark-copy-linked-files`,
-          `gatsby-remark-smartypants`,
-          `gatsby-remark-autolink-headers`,
-          `gatsby-remark-emoji`,
-        ],
-      },
-    },
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: metaConfig.title,
-        short_name: metaConfig.title,
-        start_url: `/`,
-        background_color: `#ffffff`,
-        theme_color: `#1d2740`,
-        display: `standalone`,
-        icon: metaConfig.icon,
-      },
-    },
-    {
-      resolve: `gatsby-plugin-typography`,
-      options: {
-        pathToConfigModule: `src/utils/typography`,
-      },
-    },
-    {
-      resolve: 'gatsby-plugin-robots-txt',
-      options: {
-        host: 'https://www.devjurnal.com',
-        sitemap: 'https://www.devjurnal.com/sitemap.xml',
-        policy: [
-          {
-            userAgent: '*',
-            allow: '/',
-          },
-        ],
-      },
-    },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    `gatsby-plugin-feed`,
-    `gatsby-plugin-offline`,
-    `gatsby-plugin-react-helmet`,
-    {
-        resolve: `gatsby-plugin-sass`,
-        options: {
-          implementation: require("sass"),
         },
-    },
-    `gatsby-plugin-lodash`,
-    `gatsby-plugin-sitemap`,
-    {
-        resolve: `gatsby-plugin-google-gtag`,
-        options: {
-          // You can add multiple tracking ids and a pageview event will be fired for all of them.
-          trackingIds: [
-            "G-GVQKMM834T", // Google Analytics / GA
-          ],
-          // This object is used for configuration specific to this plugin
-          pluginConfig: {
-            // Puts tracking script in the head instead of the body
-            head: true,
-          },
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                path: `${__dirname}/content/__about`,
+                name: `about`,
+            },
         },
-      },
-  ],
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                path: `${__dirname}/content/assets`,
+                name: `assets`,
+            },
+        },
+        {
+            resolve: `gatsby-transformer-remark`,
+            options: {
+                plugins: [
+                    {
+                        resolve: `gatsby-remark-katex`,
+                        options: {
+                            strict: `ignore`,
+                        },
+                    },
+                    {
+                        resolve: `gatsby-remark-images`,
+                        options: {
+                            maxWidth: 1200,
+                            linkImagesToOriginal: false,
+                        },
+                    },
+                    {
+                        resolve: `gatsby-remark-images-medium-zoom`,
+                        options: {
+                            margin: 36,
+                            scrollOffset: 0,
+                        },
+                    },
+                    {
+                        resolve: `gatsby-remark-responsive-iframe`,
+                        options: {
+                            wrapperStyle: `margin-bottom: 1.0725rem`,
+                        },
+                    },
+                    {
+                        resolve: `gatsby-remark-prismjs`,
+                        options: {
+                            inlineCodeMarker: '%',
+                        },
+                    },
+                    `gatsby-remark-copy-linked-files`,
+                    `gatsby-remark-smartypants`,
+                    `gatsby-remark-autolink-headers`,
+                    `gatsby-remark-emoji`,
+                ],
+            },
+        },
+        {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+                name: metaConfig.title,
+                short_name: metaConfig.title,
+                start_url: `/`,
+                background_color: `#ffffff`,
+                theme_color: `#1d2740`,
+                display: `standalone`,
+                icon: metaConfig.icon,
+            },
+        },
+        {
+            resolve: `gatsby-plugin-typography`,
+            options: {
+                pathToConfigModule: `src/utils/typography`,
+            },
+        },
+        {
+            resolve: 'gatsby-plugin-robots-txt',
+            options: {
+                host: 'https://mmdmthr.github.io',
+                sitemap: 'https://mmdmthr.github.io/sitemap.xml',
+                policy: [
+                    {
+                        userAgent: '*',
+                        allow: '/',
+                    },
+                ],
+            },
+        },
+        `gatsby-transformer-sharp`,
+        `gatsby-plugin-sharp`,
+        `gatsby-plugin-feed`,
+        `gatsby-plugin-offline`,
+        `gatsby-plugin-react-helmet`,
+        {
+            resolve: `gatsby-plugin-sass`,
+            options: {
+                implementation: require("sass"),
+            },
+        },
+        `gatsby-plugin-lodash`,
+        `gatsby-plugin-sitemap`,
+        {
+            resolve: `gatsby-plugin-google-gtag`,
+            options: {
+                // You can add multiple tracking ids and a pageview event will be fired for all of them.
+                trackingIds: [
+                    "G-GVQKMM834T", // Google Analytics / GA
+                ],
+                // This object is used for configuration specific to this plugin
+                pluginConfig: {
+                    // Puts tracking script in the head instead of the body
+                    head: true,
+                },
+            },
+        },
+    ],
 }
