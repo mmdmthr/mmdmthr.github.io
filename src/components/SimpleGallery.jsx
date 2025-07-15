560f57e0250a3748233aa387f340a195b0b4d876
mmdmthr <muhhari@gmail.com>
2025-07-16 06:00:07 +0700

    add more photos and lighthouse

--- src/components/SimpleGallery.jsx
+++ src/components/SimpleGallery.jsx
@@ -1,8 +1,27 @@
+import { useState } from 'react';
 import { MasonryPhotoAlbum } from 'react-photo-album';
+import Lightbox from 'yet-another-react-lightbox';
 import "react-photo-album/masonry.css";
+import "yet-another-react-lightbox/styles.css";
 
 const SimpleGallery = ({ photos }) => {
-  return <MasonryPhotoAlbum photos={photos} />;
+  const [index, setIndex] = useState(-1);
+
+  return (
+    <>
+      <MasonryPhotoAlbum
+        photos={photos}
+        onClick={({ index: current }) => setIndex(current)}
+      />
+
+      <Lightbox
+        index={index}
+        open={index >= 0}
+        close={() => setIndex(-1)}
+        slides={photos}
+      />
+    </>
+  );
 };
 
 export default SimpleGallery;
