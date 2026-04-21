44aec329b048e9258cb67ee9225638c8e4783912
mmdmthr <muhhari@gmail.com>
2026-04-21 10:55:51 +0700

    improve opengraph

--- src/consts.ts
+++ src/consts.ts
@@ -2,5 +2,5 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Dev Jurnal';
-export const SITE_DESCRIPTION = 'My notes in public space';
+export const SITE_DESCRIPTION = 'A developer blog on software development, engineering practices, and lessons learned from building real software.';
 export const GA_MEASUREMENT_ID = "G-GVQKMM834T";
